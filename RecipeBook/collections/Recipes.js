c83deb4ce2d9138ab3c2c6d2138e7558038aa43f
Wishmitha <wishmitha.14@cse.mrt.ac.lk>
2017-02-17 04:38:54 +0530

    Intermediate Tutorial #7 : Autoform completed (Added bootstrap styles to test)

--- RecipeBook/collections/Recipes.js
+++ RecipeBook/collections/Recipes.js
@@ -16,6 +16,9 @@ RecipeSchema = new SimpleSchema({
         label:"Author",
         autoValue : function () {
             return this.userId;
+        },
+        autoform:{
+            type:"hidden"
         }
 
     },
@@ -25,6 +28,8 @@ RecipeSchema = new SimpleSchema({
         label:"Created At",
         autoValue : function () {
             return new Date();
+        },autoform:{
+            type:"hidden"
         }
 
     }
